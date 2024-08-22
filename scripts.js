92a9056f67477e9f56430784b038667ab98a33b8
tamirisdepaula <tamiris01gti@gmail.com>
2024-08-22 14:51:14 -0300

    Js ok

--- scripts.js
+++ scripts.js
@@ -3,8 +3,16 @@ function generateNumber(){
 const min = Math.ceil(document.querySelector('.input-min').value)
 const max =  Math.floor(document.querySelector('.input-max').value)
 
-const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+if ( max > min ){
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
 alert(result)
 
+}
+
+else{
+    alert ('O valor minimo precisa ser MENOR que o valor maximo')
+
+}
 }
